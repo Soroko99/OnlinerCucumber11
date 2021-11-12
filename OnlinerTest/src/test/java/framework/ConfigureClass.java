6af51cd3355ebad285dadf92636c36ffe8a1c8aa
Vladislav.Saroka <Vladislav.Saroka@itechart-group.com>
2021-11-12 14:56:00 +0300

    fixes

---
+++ OnlinerTest/src/test/java/framework/ConfigureClass.java
@@ -0,0 +1,38 @@
+package framework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConfigureClass {
+    public static WebDriver driver;
+    protected static String mainUrl = "https://www.onliner.by/";
+    private String TVFiltrationXpath = "//span[@class='schema-tags__text']";
+
+    @BeforeSuite
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get(mainUrl);
+    }
+
+    @AfterSuite
+    public void driverClose(){
+        driver.quit();
+    }
+
+    public void sleep(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
