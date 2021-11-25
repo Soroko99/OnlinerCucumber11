e5d16aef15384694ca9022863cbb8089229fe2a7
Soroko99 <85428543+Soroko99@users.noreply.github.com>
2021-11-25 15:37:06 +0300

    Delete OnlinerCucumber directory

--- OnlinerCucumber/src/test/java/framework/BasePage.java
+++
@@ -1,9 +0,0 @@
-package framework;
-
-import org.openqa.selenium.WebDriver;
-
-public abstract class BasePage {
-
-    public WebDriver driver = Browser.driver;
-    public abstract void isRightPageOpenedAssertion(String title);
-}
