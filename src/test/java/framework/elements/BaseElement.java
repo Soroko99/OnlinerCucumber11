ae55bb4b6d6ba078d63886cf2fea136221c690a9
Vladislav.Saroka <Vladislav.Saroka@itechart-group.com>
2021-11-26 17:12:36 +0300

    pom changes

--- src/test/java/framework/elements/BaseElement.java
+++ src/test/java/framework/elements/BaseElement.java
@@ -47,7 +47,7 @@ public class BaseElement{
 
     public void waitForElement(){
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
     public boolean waitForIsElementPresent(){
