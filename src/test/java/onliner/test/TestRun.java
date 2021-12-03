f4f4f210fb6221e517963b2ace1c6021746d44b2
Vladislav.Saroka <Vladislav.Saroka@itechart-group.com>
2021-12-03 12:53:22 +0300

    pom changes

--- src/test/java/onliner/test/TestRun.java
+++ src/test/java/onliner/test/TestRun.java
@@ -1,8 +1,11 @@
 package onliner.test;
 
+import framework.TestListener;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 @CucumberOptions(
         features ={"src/test/java/onliner/feature/TVFilter.feature"},
         glue = "onliner/steps",
